20402701e739070a03da7fc6dd64dc265abeac79
Michael Aragon <michaeljaragon@gmail.com>
2022-05-01 04:14:54 +0000

    footer and login pages

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.feature "Visiting the homepage", type: :feature do
   scenario "The visitor should see projects" do
     visit root_path
-    expect(page).to have_text("Home 🏠\nIt's Michael Aragon!\nMichael develops software for Ent Credit Union. He has a BS in Mathematics from UCCS and will be starting my MS in Computer Science in the Fall 2022.\nGithub »\nMichael Aragon 🙂 | michaeljaragon@gmail.com")
+    expect(page).to have_text("Home 🏠\nIt's Michael Aragon!\nMichael develops software for Ent Credit Union. He has a BS in Mathematics from UCCS and will be starting his MS in Computer Science in Fall 2022.\nGithub »\nMichael Aragon 🙂 | michaeljaragon@gmail.com")
   end
 end
